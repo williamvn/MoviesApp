86fcb6946c5880789319de1a046065fa868b4e87
Willy <williamvn1995@gmail.com>
2020-07-09 11:27:58 +0200

    ngOnInit method implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { TvShowsComponent } from './tv-shows/tv-shows.component';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
   {path:"movies", component: MoviesComponent},
-  {path:"tv-shows", component:TvShowsComponent}
+  {path:"tv-shows", component:TvShowsComponent},
+  {path:":product/:productId", component:ProductDetailsComponent}
 ];
 
 @NgModule({
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductDetails } from '../shared/model/product-detail';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product:ProductDetails;
+  constructor(private route: ActivatedRoute, private data:DataService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      var productType = params.get('product');
+      var productId = +params.get('productId');
+      if(productType === "movie"){
+        this.product = this.data.movies.find(m=>m.id == productId)[0];
+      }
+      else{
+        this.product = this.data.tvshows.find(tv=>tv.id == productId)[0];
+      }
+    });
+
   }
 
 }
