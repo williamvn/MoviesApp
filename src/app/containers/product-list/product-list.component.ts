6ded091fc6da9d6ff513f94aef2a1f9a998a0d29
Willy <williamvn1995@gmail.com>
2020-07-11 18:33:58 +0200

    no video Availabe Add

--- src/app/containers/product-list/product-list.component.ts
+++ src/app/containers/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Product } from '../../model/product';
 
 @Component({
@@ -6,12 +6,16 @@ import { Product } from '../../model/product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnChanges {
   @Input()
   products:Product[];
   @Input()
   type:string;
   constructor() { }
+
+  ngOnChanges(){
+    console.log(this.products);
+  }
 
   ngOnInit(): void {
   }
